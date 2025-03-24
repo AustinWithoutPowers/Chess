f5759055b9b621874dd26c3cea55c6aecaf6e76e
AustinWithoutPowers <austinrudkin@live.com>
2025-03-24 16:56:51 +1300

    Did lots, building out classes and added some nice, consistent testing

--- core/Classes.py
+++ core/Classes.py
@@ -1,6 +1,32 @@
 # https://en.wikipedia.org/wiki/Portable_Game_Notation
 
+
+# Class template
+'''
+class Name:
+    # Constants
+
+    # Dunder functions
+    def __init__(self):
+        pass
+    
+    def __str__(self):
+        pass
+    
+    def __repr__(self):
+        return str(self)
+    
+    # Private functions
+
+    # Getters
+
+    # Setters
+
+    # External functions
+'''
+
 class Piece:
+    # Constants
     WHITE = 'W'
     BLACK = 'B'
 
@@ -11,7 +37,14 @@ class Piece:
     QUEEN = 'Q'
     KING = 'K'
 
+    # Dunder functions
     def __init__(self, side, type):
+        if not self.__check_side(side):
+            raise Exception('Side is incorrect.')
+        
+        if not self.__check_type(type):
+            raise Exception('Type is incorrect.')
+        
         self.__side = side
         self.__type = type
 
@@ -41,20 +74,117 @@ class Piece:
 
     def __repr__(self):
         return str(self)
+
+    def __eq__(self, other):
+        if self.get_side() == other.get_side() and \
+        self.get_type() == other.get_type():
+            return True
+        return False
+
+    def __ne__(self, other):
+        return not self == other
+    
+    # Private functions
+    def __check_side(self, side_input):
+        if side_input not in [self.WHITE, self.BLACK]:
+            return False
+        return True
+    
+    def __check_type(self, type_input):
+        if type_input not in [self.PAWN, self.KNIGHT, \
+                              self.BISHOP, self.ROOK, \
+                                self.QUEEN, self.KING]:
+            return False
+        return True
+    
+    # Getters
+    def get_side(self):
+        return self.__side
+    
+    def get_type(self):
+        return self.__type
+    
+    # Setters
+
+    # External functions
     
 class Square:
-    def __init__(self):
-        pass
+    # Constants
+    FILE_ARRAY = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
+    # Dunder functions
+    def __init__(self, square_str):
+        file, rank = self.__convert_square_str(square_str)
+        
+        self.__file_index = self.__encode_file(file)
+        self.__rank_index = rank - 1 # Keep this as int
+        self.__piece = None
+    
+    def __str__(self):
+        return self.get_file() + str(self.get_rank())
+
+    def __repr__(self):
+        return str(self)
+    
+    def __eq__(self, other):
+        if self.get_file() == other.get_file() and \
+            self.get_rank() == other.get_rank():
+            return True
+    
+    def __ne__(self, other):
+        return not (self == other)
+
+    # Private functions
+    def __check_file(self, file):
+        if file not in self.FILE_ARRAY:
+            raise Exception('File is incorrect.')
+    
+    def __check_rank(self, rank):
+        if rank < 1 or rank > 8:
+            raise Exception('Rank is incorrect.')
+
+    def __convert_square_str(self, square_str):
+        if len(square_str) != 2:
+            raise Exception('Square is incorrect.')
+        self.__check_file(square_str[0])
+        self.__check_rank(int(square_str[1]))
+        
+        return square_str[0], int(square_str[1])
+
+    def __encode_file(self, file):
+        return self.FILE_ARRAY.index(file)
+    
+    def __decode_file(self, file_index):
+        return self.FILE_ARRAY[file_index]
+    
+    # Getters
+    def get_file(self):
+        return self.__decode_file(self.__file_index)
+
+    def get_rank(self):
+        return self.__rank_index + 1
+    
+    def get_piece(self):
+        return self.__piece
+    
+    # Setters
+    def set_piece(self, piece):
+        self.__piece = piece
+    
+    # External functions
 
 class Board:
+    # Constants
+
+    # Dunder functions
     def __init__(self):
         self.__board_array = []
-        for i in range(8):
-            temp_array = []
-            for j in range(8):
-                temp_array += [None]
-            self.__board_array += [temp_array]
+
+        for rank in range(1, 9):
+            for file in Square.FILE_ARRAY:
+                self.__board_array += [Square(file + str(rank))]
+        
+        self.__standard_setup()
     
     def __str__(self):
         return str(self.__board_array)
@@ -62,19 +192,24 @@ class Board:
     def __repr__(self):
         str(self)
     
+    # Private functions
+    def __get_board_square(self, input_square):
+        for square in self.__board_array:
+            if input_square == square:
+                return square
+
+    def __standard_setup(self):
+        # Kings
+        self.__get_board_square(Square('e1')).set_piece(Piece(Piece.WHITE, Piece.KING))
+        self.__get_board_square(Square('e8')).set_piece(Piece(Piece.BLACK, Piece.KING))
+
+    # Getters
     def get_piece(self, square):
-        pass
-    
-    def file_index(self, file):
-        return ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'].index(file)
+        return self.__get_board_square(square).get_piece()
 
-    def rank_index(self, rank):
-        # Probably don't need self, but it's consistent?
-        return rank - 1
+    def set_piece(self, square, piece):
+        self.__get_board_square(square).set_piece(piece)
 
-    def standard_setup(self):
-        # Add kings
-        self.__board_array[self.rank_index(1)][self.file_index('e')] = \
-            Piece(Piece.WHITE, Piece.KING)
-        self.__board_array[self.rank_index(8)][self.file_index('e')] = \
-            Piece(Piece.BLACK, Piece.KING)
+    # Setters
+
+    # External functions
--- test.py
+++ test.py
@@ -1,46 +1,171 @@
 import unittest
-from core.classes import Piece, Board
+from core.classes import *
+
+# Test class template
+'''
+class NameTests(unittest.TestCase):
+    # Declare some testing variables
+    try:
+        a = Name()
+    except Exception as e:
+        print('Failed to create Name: ', e)
+
+    # Tests for creation
+    
+    # Tests for dunder functions
+
+    # Tests for getters
+    
+    # Tests for setters
+    
+    # Tests for external functions
+'''
 
 class PieceTests(unittest.TestCase):
-    def test_piece_creation_pawn(self):
-        pawn = Piece(Piece.WHITE, Piece.PAWN)
-        self.assertEqual(str(pawn), 'White pawn')
+    # Declare some testing variables
+    try:
+        white_pawn = Piece(Piece.WHITE, Piece.PAWN)
 
-    def test_piece_creation_knight(self):
-        knight = Piece(Piece.BLACK, Piece.KNIGHT)
-        self.assertEqual(str(knight), 'Black knight')
+        # Ahaha...
+        # bishop_black = Piece(Piece.BISHOP, Piece.BLACK)
+    except Exception as e:
+        print('Uh oh! Failed to create Pieces: ', e)
+    
+    # Tests for creation
+    def test_piece_create_args_count_low(self):
+        with self.assertRaises(Exception) as error:
+            Piece(Piece.WHITE)
+        self.assertRaises(TypeError, error)
 
-    def test_piece_creation_bishop(self):
-        bishop = Piece(Piece.WHITE, Piece.BISHOP)
-        self.assertEqual(str(bishop), 'White bishop')
+    def test_piece_create_args_count_high(self):
+        with self.assertRaises(Exception) as error:
+            Piece(Piece.WHITE, Piece.PAWN, Piece.KING)
+        self.assertRaises(TypeError, error)
 
-    def test_piece_creation_rook(self):
-        rook = Piece(Piece.BLACK, Piece.ROOK)
-        self.assertEqual(str(rook), 'Black rook')
+    def test_piece_create_wrong_side(self):
+        with self.assertRaises(Exception) as error:
+            Piece('V', Piece.PAWN)
+        self.assertTrue('Side is incorrect.' in str(error.exception))
+    
+    def test_piece_create_wrong_type(self):
+        with self.assertRaises(Exception) as error:
+            Piece(Piece.WHITE, 'J')
+        self.assertTrue('Type is incorrect.' in str(error.exception))
 
-    def test_piece_creation_queen(self):
-        queen = Piece(Piece.WHITE, Piece.QUEEN)
-        self.assertEqual(str(queen), 'White queen')
+    # Tests for dunder functions
+    def test_piece_str(self):
+        self.assertEqual(str(self.white_pawn), 'White pawn')
+    
+    def test_piece_eq(self):
+        self.assertEqual(self.white_pawn, Piece(Piece.WHITE, Piece.PAWN))
+    
+    def test_piece_ne_side(self):
+        self.assertNotEqual(self.white_pawn, Piece(Piece.BLACK, Piece.PAWN))
+    
+    def test_piece_ne_side(self):
+        self.assertNotEqual(self.white_pawn, Piece(Piece.WHITE, Piece.BISHOP))
 
-    def test_piece_creation_king(self):
-        king = Piece(Piece.BLACK, Piece.KING)
-        self.assertEqual(str(king), 'Black king')
+    # Tests for getters
+    def test_piece_get_side(self):
+        self.assertEqual(self.white_pawn.get_side(), Piece.WHITE)
 
-EMPTY_BOARD = '[[None, None, None, None, None, None, None, None], \
-    [None, None, None, None, None, None, None, None], \
-        [None, None, None, None, None, None, None, None], \
-            [None, None, None, None, None, None, None, None], \
-                [None, None, None, None, None, None, None, None], \
-                    [None, None, None, None, None, None, None, None], \
-                        [None, None, None, None, None, None, None, None], \
-                            [None, None, None, None, None, None, None, None]]'
+    def test_piece_get_type(self):
+        self.assertEqual(self.white_pawn.get_type(), Piece.PAWN)
+    
+    # Tests for setters
+
+    # Tests for external functions
+
+class SquareTests(unittest.TestCase):
+    # Declare some testing variables
+    try:
+        d4 = Square('d4') # Great square
+    except Exception as e:
+        print('Failed to create Squares: ', e)
+
+    # Tests for creation
+    def test_square_create_str(self):
+        self.assertEqual(str(self.d4), 'd4')
+    
+    def test_square_create_piece(self):
+        self.assertIsNone(self.d4.get_piece())
+    
+    def test_square_create_wrong_file(self):
+        with self.assertRaises(Exception) as error:
+            Square('i1')
+        self.assertTrue('File is incorrect.' in str(error.exception))
+
+    def test_square_create_wrong_rank_less(self):
+        with self.assertRaises(Exception) as error:
+            Square('a0')
+        self.assertTrue('Rank is incorrect.' in str(error.exception))
+    
+    def test_square_create_wrong_rank_more(self):
+        with self.assertRaises(Exception) as error:
+            Square('a9')
+        self.assertTrue('Rank is incorrect.' in str(error.exception))
+    
+    # Tests for dunder functions
+    def test_square_str(self):
+        self.assertEqual(str(self.d4), 'd4')
+    
+    def test_square_eq(self):
+        self.assertEqual(self.d4, Square('d4'))
+
+    # Tests for getters
+    def test_square_get_file(self):
+        self.assertEqual(self.d4.get_file(), 'd')
+
+    def test_square_get_rank(self):
+        self.assertEqual(self.d4.get_rank(), 4)
+    
+    def test_square_get_piece(self):
+        self.assertIsNone(self.d4.get_piece())
+    
+    # Tests for setters
+    def test_square_set_piece(self):
+        e4 = Square('e4')
+        e4.set_piece(Piece(Piece.WHITE, Piece.PAWN))
+        self.assertEqual(str(e4.get_piece()), 'White pawn')
+    
+    # Tests for external functions
 
 class BoardTests(unittest.TestCase):
-    def test_empty_board_creation(self):
-        self.assertEqual(str(Board()), '[[None, None, None, None, None, None, None, None], [None, None, None, None, None, None, None, None], [None, None, None, None, None, None, None, None], [None, None, None, None, None, None, None, None], [None, None, None, None, None, None, None, None], [None, None, None, None, None, None, None, None], [None, None, None, None, None, None, None, None], [None, None, None, None, None, None, None, None]]')
+    # Declare some testing variables
+    try:
+        board = Board()
+    except Exception as e:
+        print('Failed to create Boards: ', e)
+
+    # Tests for creation
+    # No arguments
     
-    def test_white_king_position(self):
-        self.assertEqual(Board())
+    # Tests for dunder functions
+    def test_board_str(self):
+        self.assertTrue('e4, f4, g4, h4, a5, b5, c5, d5' in str(self.board))
+
+    # Tests for getters
+    def test_board_get_piece(self):
+        self.assertEqual(self.board.get_piece(Square('e1')), Piece(Piece.WHITE, Piece.KING))
+
+    # Tests for setters
+    def test_board_set_piece(self):
+        temp_board = Board()
+        temp_square = Square('e1')
+
+        temp_board.set_piece(temp_square, Piece(Piece.BLACK, Piece.QUEEN))
+
+        self.assertEqual(temp_board.get_piece(temp_square), Piece(Piece.BLACK, Piece.QUEEN))
+    
+    # Tests for external functions
+
+
+
+def ad_hoc_test():
+    print('-:[ AD HOC TESTS STARTING ]:-')
+
+    print('-:[  AD HOC TESTS ENDING  ]:-')
 
 if __name__ == '__main__':
+    ad_hoc_test()
     unittest.main()
